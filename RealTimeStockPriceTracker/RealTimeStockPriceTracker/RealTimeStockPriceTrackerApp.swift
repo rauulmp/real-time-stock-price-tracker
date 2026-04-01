44a09c2c9f62f5adb5ea8d253d266ec5f347ba51
Raul Montoya Perez <raulomontoyaperez@gmail.com>
2026-04-02 00:26:32 +0200

    Initial project

---
+++ RealTimeStockPriceTracker/RealTimeStockPriceTracker/RealTimeStockPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeStockPriceTrackerApp.swift
+//  RealTimeStockPriceTracker
+//
+//  Created by Raul Montoya Perez on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeStockPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RealTimeStockPriceTracker/RealTimeStockPriceTrackerTests/RealTimeStockPriceTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeStockPriceTrackerTests.swift
+//  RealTimeStockPriceTrackerTests
+//
+//  Created by Raul Montoya Perez on 2/4/26.
+//
+
+import Testing
+@testable import RealTimeStockPriceTracker
+
+struct RealTimeStockPriceTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
